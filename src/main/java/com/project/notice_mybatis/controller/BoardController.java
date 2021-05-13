af798ef5b771e5cb649dc63d645f2de59ac0aceb
rrakoon <lth0285@gmail.com>
2021-05-13 16:34:08 +0900

    insert complete

--- src/main/java/com/project/notice_mybatis/controller/BoardController.java
+++ src/main/java/com/project/notice_mybatis/controller/BoardController.java
@@ -18,11 +18,11 @@ public class BoardController {
     @Autowired
     private BoardService boardService;
 
-    @GetMapping(value = "/write")
     //@RequestParam : view에서 전달받은 파라미터 처리하는데 사용.
     //value="값" = > view에서 받을 파라미터 값.
     //required = false => 지정 키값이 없을때 Bad Request Error발생 방지. default값은 true며 반드시 false로 고친다.
     //defaultValue = "값" => 지정 키값이 존재 안하면 기본값 할당.
+    @GetMapping(value = "/write")
     public String openBoardWrite(@RequestParam(value = "idx", required = false) Long idx, Model model) {
         if (idx == null) {
             model.addAttribute("board", new BoardDTO());
@@ -41,13 +41,15 @@ public class BoardController {
         try {
             boolean isRegistered = boardService.registerBoard(params);
             if (isRegistered == false) {
-                //TODO => 게시글 등록에 실패하였다는 메시지를 전달
+                // TODO => 게시글 등록에 실패하였다는 메시지를 전달
             }
         } catch (DataAccessException e) {
-            //TODO => 데이터베이스 처리 과정에 문제가 발생하였다는 메시지를 전달
+            // TODO => 데이터베이스 처리 과정에 문제가 발생하였다는 메시지를 전달
+
         } catch (Exception e) {
-            //TODO => 시스템에 문제가 발생하였다는 메시지를 전달
+            // TODO => 시스템에 문제가 발생하였다는 메시지를 전달
         }
+
         return "redirect:/board/list";
     }
 
