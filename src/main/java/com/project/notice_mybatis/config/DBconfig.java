956369cfbfea6ce1ab0131a29189055f07b46e6a
rrakoon <lth0285@gmail.com>
2021-05-13 13:19:58 +0900

    crud test complete

--- src/main/java/com/project/notice_mybatis/config/DBconfig.java
+++ src/main/java/com/project/notice_mybatis/config/DBconfig.java
@@ -34,7 +34,8 @@ public class DBconfig {
     public SqlSessionFactory sqlSessionFactory() throws Exception {
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
         factoryBean.setDataSource(dataSource());
-		factoryBean.setMapperLocations(applicationContext.getResources("classpath:/mybatis-mappers/**/*.xml"));
+//		factoryBean.setMapperLocations(applicationContext.getResources("classpath:/mybatis-mappers/**/*.xml"));
+		factoryBean.setMapperLocations(applicationContext.getResources("classpath:/mybatis-mappers/*.xml"));
         factoryBean.setTypeAliasesPackage("com.project.notice_mybatis.domain");
         factoryBean.setConfiguration(mybatisConfg());
 		return factoryBean.getObject();
